1506af5e35aeecb2ef418c21f7768e7a5865cc67
EasonLiao <liaoyisheng89@gmail.com>
2015-02-02 23:15:33 -0800

    Format (deleted/modified/new file) on status message

--- builtin/status.go
+++ builtin/status.go
@@ -30,13 +30,13 @@ func CmdStatus() error {
   if len(deleted) > 0 || len(newFiles) > 0 || len(diffes) > 0 {
     fmt.Println("Changes to be committed:\n")
     for _, file := range(deleted) {
-      fmt.Printf("\t%s\n", TreePathToRelFsPath(file))
+      fmt.Printf("\tdeleted:\t%s\n", TreePathToRelFsPath(file))
     }
     for _, file := range(newFiles) {
-      fmt.Printf("\t%s\n", TreePathToRelFsPath(file))
+      fmt.Printf("\tnew file:\t%s\n", TreePathToRelFsPath(file))
     }
     for _, file := range(diffes) {
-      fmt.Printf("\t%s\n", TreePathToRelFsPath(file))
+      fmt.Printf("\tmodified:\t%s\n", TreePathToRelFsPath(file))
     }
     fmt.Println("")
   }
@@ -46,16 +46,16 @@ func CmdStatus() error {
   if len(deleted) > 0 || len(diffes) > 0 {
     fmt.Println("Changes not statged for commit:\n")
     for _, file := range(deleted) {
-      fmt.Printf("\t%s\n", TreePathToRelFsPath(file))
+      fmt.Printf("\tdeleted:\t%s\n", TreePathToRelFsPath(file))
     }
     for _, file := range(diffes) {
-      fmt.Printf("\t%s\n", TreePathToRelFsPath(file))
+      fmt.Printf("\tmodified:\t%s\n", TreePathToRelFsPath(file))
     }
     fmt.Println("")
   }
 
   if len(untracked) > 0 {
-    fmt.Println("Untracked files:\n")
+    fmt.Println("Untracked files/directories:\n")
     for _, file := range(untracked) {
       fmt.Printf("\t%s\n", TreePathToRelFsPath(file))
     }
